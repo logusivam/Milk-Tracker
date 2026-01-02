0f9e748f5be0761f18bfc08eded106f587f4c5ab
Loganathan G P <106108501+logusivam@users.noreply.github.com>
2026-01-02 09:57:22 +0530

    cache ttl set to 1 hr updated

--- backend/src/utils/cache.js
+++ backend/src/utils/cache.js
@@ -1,7 +1,7 @@
 import NodeCache from "node-cache";
 
 const cache = new NodeCache({
-  stdTTL: 36,       // default TTL: 1 hour  // TODO: need to update to 3600 (in production)
+  stdTTL: 3600,       // default TTL: 1 hour  
   checkperiod: 120,
   useClones: false   // IMPORTANT: avoid deep clone overhead
 });
