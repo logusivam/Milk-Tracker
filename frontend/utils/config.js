0b644cd4985049aae549ec671dd4ef38d058dff4
Loganathan G P <106108501+logusivam@users.noreply.github.com>
2026-04-17 12:49:53 +0530

    fix: update API_BASE_URL to use the correct production endpoint

--- frontend/utils/config.js
+++ frontend/utils/config.js
@@ -2,5 +2,5 @@
 const isProd = window.location.protocol === 'https:';
  
 export const API_BASE_URL = isProd ? 
-        "https://milk-tracker-api.onrender.com" : //https://milk-tracker-be.onrender.com
+        "https://milk-tracker-be.onrender.com" : // ,https://milk-tracker-api.onrender.com 
         "http://localhost:5000";
